e287a3586d920f0e8b8b682f4b86dab81ea38401
Ray He <rayhe921@gmail.com>
2021-02-18 21:55:10 -0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -14,42 +14,42 @@ class App extends Component {
 
   componentDidMount() {
   }
-   activeSidebar = (event) => {
-      event.preventDefault();
-      this.state.sidebarButton === true ? this.setState({sidebarButton: false})
-       :
-      this.setState({sidebarButton: true})
-    }; 
-  
+  activeSidebar = (event) => {
+    event.preventDefault();
+    this.state.sidebarButton === true ? this.setState({ sidebarButton: false })
+      :
+      this.setState({ sidebarButton: true })
+  };
+
 
   render() {
     return (
       <Router>
         <div className="wrapper">
-        <div>
-        <Sidebar
-          title="Raymond He"
-          email="Email"
-          github="Github"
-          linkedIn="LinkedIn"
-          resume="Resume"
-          linkOne="mailto:rayhe921@gmail.com"
-          linkTwo="https://github.com/rayhe921"
-          linkThree="https://www.linkedin.com/in/raymond-he-7086545a"
-          linkFour="https://drive.google.com/file/d/1JUPCPCCRIVjmAfSGsqsHot-cjqi9ZTSb/view?usp=sharing"
-          closeSide={this.state.sidebarButton}
-        ></Sidebar>
-        </div>
-        <div className="content">
-        <Navbar
-         onClick={this.activeSidebar}
-         title="Toggle Menu"
-        ></Navbar>
-        <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-        </Switch>
-        </div>
+          <div>
+            <Sidebar
+              title="Raymond He"
+              email="Email"
+              github="Github"
+              linkedIn="LinkedIn"
+              resume="Resume"
+              linkOne="mailto:rayhe921@gmail.com"
+              linkTwo="https://github.com/rayhe921"
+              linkThree="https://www.linkedin.com/in/raymond-he-7086545a"
+              linkFour="https://drive.google.com/file/d/1JUPCPCCRIVjmAfSGsqsHot-cjqi9ZTSb/view?usp=sharing"
+              closeSide={this.state.sidebarButton}
+            ></Sidebar>
+          </div>
+          <div className="content">
+            <Navbar
+              onClick={this.activeSidebar}
+              title="Toggle Menu"
+            ></Navbar>
+            <Switch>
+              <Route exact path="/" component={About} />
+              <Route exact path="/portfolio" component={Portfolio} />
+            </Switch>
+          </div>
         </div>
       </Router>
     );
