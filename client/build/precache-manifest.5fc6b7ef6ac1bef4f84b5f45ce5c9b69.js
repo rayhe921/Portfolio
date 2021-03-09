b947fa93f47241d23a5d26ccc196efa74f442379
Ray He <rayhe921@gmail.com>
2021-03-08 21:12:50 -0800

    build for deploy

---
+++ client/build/precache-manifest.5fc6b7ef6ac1bef4f84b5f45ce5c9b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96386f02f436f0bed51",
+    "url": "/static/css/main.7bcf2b50.chunk.css"
+  },
+  {
+    "revision": "c96386f02f436f0bed51",
+    "url": "/static/js/main.a17d85d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7c317dcfd69e0018f6b",
+    "url": "/static/js/2.3eb1fc64.chunk.js"
+  },
+  {
+    "revision": "d8dde6f9a841ddbee72f7a1e1aafecea",
+    "url": "/static/media/portrait.d8dde6f9.jpg"
+  },
+  {
+    "revision": "80fc94b74e493dc2ad2ce6daae1e70dd",
+    "url": "/index.html"
+  }
+];
