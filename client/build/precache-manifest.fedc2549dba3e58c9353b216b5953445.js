47b133170f91d89eac66ecb3c840a5f8bb8cd4b5
Ray He <rayhe921@gmail.com>
2021-03-10 08:45:30 -0800

    checkout reset

---
+++ client/build/precache-manifest.fedc2549dba3e58c9353b216b5953445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4fc81861220011d2a2",
+    "url": "/static/css/main.436d9155.chunk.css"
+  },
+  {
+    "revision": "9f4fc81861220011d2a2",
+    "url": "/static/js/main.5d5b9e80.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7c317dcfd69e0018f6b",
+    "url": "/static/js/2.3eb1fc64.chunk.js"
+  },
+  {
+    "revision": "d8dde6f9a841ddbee72f7a1e1aafecea",
+    "url": "/static/media/portrait.d8dde6f9.jpg"
+  },
+  {
+    "revision": "98ae5b3712365f124a96db32210acab3",
+    "url": "/index.html"
+  }
+];
